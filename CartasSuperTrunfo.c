e834a6311bbba84d1e8eb6d15dcb26c0f5ab02a1
Rodrigo Alves Nonaka <77215088+apeiou@users.noreply.github.com>
2025-03-24 21:02:07 +0000

    Adição da entrada/saída dos dados da segunda carta; Edição do código para melhor visualização

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,9 +24,34 @@ int main(){
     printf("Digite o número de pontos turísticos: \n");
     scanf(" %d", &pontoTur1);
 
+    //dados da carta 2
+    char estado2, codigo2[5], cidade2[50];
+    int populacao2, pontoTur2;
+    float area2, pib2;
+
+    //entrada dos dados da carta 2
+    printf("Insira os dados da segunda carta \n");
+    printf("Digite a letra do estado: \n");
+    scanf(" %c", &estado2);
+    printf("Digite o código da carta: \n");
+    scanf(" %s", codigo2);
+    printf("Digite o nome da cidade: \n");
+    scanf(" %s", cidade2);
+    printf("Digite o número da população: \n");
+    scanf(" %d", &populacao2);
+    printf("Digite a área (em km²): \n");
+    scanf(" %f", &area2);
+    printf("Digite o PIB: \n");
+    scanf(" %f", &pib2);
+    printf("Digite o número de pontos turísticos: \n");
+    scanf(" %d", &pontoTur2);
+
     //visualização dos dados da carta 1
-    printf("Dados da primeira carta: \n");
+    printf("\nA primeira carta é: \n");
     printf("Estado: %c \nCódigo: %s \nCidade: %s \nPopulação: %d \nÁrea: %.2f km² \nPIB: %.2f bilhões de reais\nPontos turísticos: %d \n", estado1, codigo1, cidade1, populacao1, area1, pib1, pontoTur1);
+    //visualização dos dados da carta 2
+    printf("\nA segunda carta é: \n");
+    printf("Estado: %c \nCódigo: %s \nCidade: %s \nPopulação: %d \nÁrea: %.2f km² \nPIB: %.2f bilhões de reais\nPontos turísticos: %d \n", estado2, codigo2, cidade2, populacao2, area2, pib2, pontoTur2);
 
     return 0;
 }
